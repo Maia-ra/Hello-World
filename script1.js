187fb129993b0144d9a6cf9606e901691f19d9c9
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-02 20:00:01 -0300

    feat: adiciona variável para armazenar um valor

--- script1.js
+++ script1.js
@@ -3,3 +3,11 @@ console.log(myName);
 // Output: Maiara
 
 var favoriteFood = 'Pizza';
+// Armazena nome da comida favorita
+console.log(favoriteFood);
+// Output: Pizza
+
+var numOfSlices = 8;
+// Armazena o número 8
+console.log(numOfSlices);
+// Output: 8
