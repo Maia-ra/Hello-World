135984afa54389b021050eb1d0ec6faad12d13cf
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-02 20:39:26 -0300

    feat: trabalhando com operadores matematicos

--- script2.js
+++ script2.js
@@ -2,3 +2,8 @@ const myName = 'Maiara';
 console.log(myName);
 // Variáveis do tipo Constante (Const) não podem ser reatribuidas
 //Pois geram erro no código
+
+let w = 4;
+w = w + 1;
+
+console.log(w); // Output: 5
