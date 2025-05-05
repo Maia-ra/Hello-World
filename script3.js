e6ba1effd6e22275f7ebe0d34d6212fb8c5e653e
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-04 21:55:12 -0300

    chore: adiciona novo arquivo javascript

---
+++ script3.js
@@ -0,0 +1,22 @@
+let a = 10;
+a++;
+console.log(a); // Output = 11
+
+let b = 20;
+b--;
+console.log(b); // Output = 19
+
+let gainedDollar = 3;
+gainedDollar++;
+console.log(gainedDollar); // Output = 4
+
+let lostDollar = 50;
+lostDollar--;
+console.log(lostDollar); // Output = 49
+
+let myPet = 'armadillo';
+console.log('I own a pet ' + myPet + '.');
+// Output = 'I own a pet armadillo.'
+
+let favoriteAnimal = 'Dog';
+console.log('My favorite animal: ' + favoriteAnimal);
