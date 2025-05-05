23ff7e0fbdb5dfda5c407f84fed1ce7ca4bdbddd
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-04 22:58:54 -0300

    feat: Testando estruturas condicionais

--- script5.js
+++ script5.js
@@ -2,3 +2,10 @@ if (true){
     console.log('This message will print!');
 }
 // Prints: This message will print!
+
+let sale = true;
+sale = false;
+
+if(sale) {
+    console.log('Time to buy!');
+}
