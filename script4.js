bf33f818b15dd0024fa3bee006bae2d3fd0307c5
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-04 22:07:07 -0300

    feat: adiciona variaveis constantes ao arquivo .js

--- script4.js
+++ script4.js
@@ -5,3 +5,11 @@ console.log(`I own a pet ${myPet}.`);
 const unknown1 = 'foo';
 console.log(typeof unknown1);
 // output = string
+
+const unknown2 = 10;
+console.log(typeof unknown2);
+// Output = number 
+
+const unknown3 = true;
+console.log(typeof unknown3);
+// Output = boolean
