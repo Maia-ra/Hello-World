eba901ae9cfc02b570f9596884a5198bedc4d4ce
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-05 22:28:49 -0300

    chore: adiciona mais um arquivo js

---
+++ script7.js
@@ -0,0 +1,11 @@
+if (stopLight === 'green' && pedestrians === 0){
+    console.log('Go!');
+} else {
+    console.log('Stop');
+}
+
+if (day === 'Saturday' || day === 'Sunday') {
+    console.log('Enjoy the weekend!');
+} else {
+    console.log('Do some work.');
+}
