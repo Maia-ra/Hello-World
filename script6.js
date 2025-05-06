6450fc19c7bfe8e3c37b15617bb03b62cc6baac4
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-05 22:21:44 -0300

    chore: adiciona novo arquivo .js

---
+++ script6.js
@@ -0,0 +1,7 @@
+if (false) {
+    console.log('The code is this block will not run.');
+} else {
+    console.log('But the code in this block will!');
+}
+
+// Prints: But the code in this block will!
