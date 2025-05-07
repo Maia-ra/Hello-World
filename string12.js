bbff5711d517dec207a35cbda462f4a31712fe08
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-06 21:14:16 -0300

    chore: Novo arquivo js adicionado simplificando o código anterior

---
+++ string12.js
@@ -0,0 +1,5 @@
+isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the lights!');
+
+// Simplificando bloco de if e else
+// É verificado se a primeira condição é verdadeira, sendo ela é executada
+// A primeira sendo falsa, o segundo é executado
