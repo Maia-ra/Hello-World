14bd5894ab989efd5c12b4def49c25c538245b26
Maia-ra <maiara_rodriguesj@hotmail.com>
2025-05-06 21:02:56 -0300

    chore: Adiciona mais um arquivo javascript

---
+++ script9.js
@@ -0,0 +1,13 @@
+let username = '';
+let defaultName;
+
+if (username) {
+    defaultName = username;
+} else {
+    defaultName = 'Stranger';
+}
+
+console.log(defaultName);
+// Prints: Stranger
+
+
